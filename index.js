baeccd1cac41b97ceff99361040e332dc182103c
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-08-26 18:27:45 -0300

    Added the questions to an question array

---
+++ index.js
@@ -0,0 +1,7 @@
+const question = [
+    'Did you eat right?',
+    'Did you go to the gym?',
+    'Did you drink enough water',
+    'Have you done any commits?',
+    'Have you talked to your family?'
+]
