17d0685fe45cc58a21dc7235a15f4c0887974138
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-08-27 14:34:27 -0300

    Now the application shows a summary of all the questions and answers

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ function ask (index = 0) {
 process.stdin.on('data', (data) => {
     answers.push(data.toString().trim())
     if (questions.length > answers.length) {
+        console.log("")
         ask(answers.length)
     } else {
+        console.log("\n\n\n")
+        console.log("Summary: \n")
+        for (let i = 0; i < questions.length; i++) {
+            console.log(`${questions[i]} --> ${answers[i]} \n`)
+        }
         process.exit()
     }
 })
