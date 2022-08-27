ba5734ffdf399c60551b34e350bb161234f15f08
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-08-27 14:23:28 -0300

    Now all the questions are displayes until the answers array isn't smaller than the questions one

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const question = [
+const questions = [
     'Did you eat right?',
     'Did you go to the gym?',
     'Did you drink enough water',
     'Have you done any commits?',
     'Have you talked to your family?'
 ]
+
+const answers = []
+
+function ask (index = 0) {
+    process.stdout.write(questions[index] + '\n')
+}
+
+process.stdin.on('data', (data) => {
+    answers.push(data.toString().trim())
+    if (questions.length > answers.length) {
+        ask(answers.length)
+    } else {
+        process.exit()
+    }
+})
+
+ask()
